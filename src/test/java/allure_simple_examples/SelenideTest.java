cc48432955f35f92798d41c059f5a5c6a10561b2
demius6 <yakimovich.99@mail.ru>
2024-03-20 21:47:20 +0300

    github test

--- src/test/java/allure_simple_examples/SelenideTest.java
+++ src/test/java/allure_simple_examples/SelenideTest.java
@@ -1,2 +1,33 @@
-package allure_simple_examples;public class SelenideTest {
+package allure_simple_examples;
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+
+public class SelenideTest {
+
+    @Test
+    public void searchForIssue() {
+//        Configuration.remote = "http://localhost:4444/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example ");
+        $("#query-builder-test").submit();
+        $(By.linkText("Repositories")).click();
+        $(By.linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#87")).should(Condition.exist);
+    }
 }
